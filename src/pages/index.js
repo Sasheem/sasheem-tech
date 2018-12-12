03cdcc2b8fbd1be275ba6ccf19372f4323ac6bfe
Sasheem <sts12@me.com>
2018-12-12 00:37:15 -0500

    added spacing to layout for profile section with flex

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,24 @@ import styled from 'styled-components';
 
 import Layout from '../components/layout'
 import Image from '../components/image'
-// import ProfileImage from '../components/profile-image';
 
 const ProfileContainer = styled.div`
-  height: 80vh;
-  width: 80vw;
+  height: 88vh;
+  width: 100vw;
   display: flex;
-  background: yellow;
+  align-items: center;
+  justify-content: center;
 `;
 
 const IndexPage = () => (
   <Layout>
     <ProfileContainer>
-      <div style={{ background: 'blue' }}>
+      <div style={{ marginRight: '5em' }}>
         <h1>Hi I'm Sasheem</h1>
         <p>Mobile App and Web Developer</p>
         <p>I build things with React and React Native</p>
       </div>
-      <div style={{ maxWidth: '300px', marginBottom: '1.45rem', width: '300px' }}>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem', width: '300px', marginLeft: '5em' }}>
         <Image />
       </div>
     </ProfileContainer>
