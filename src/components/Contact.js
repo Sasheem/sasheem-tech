fa5b93753e8c59f5371a3783663656f8ca7e593a
Sasheem <sts12@me.com>
2018-12-17 21:35:52 -0500

    added form elements with flex

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FormRow = styled.p`
+    display: flex;
+    justify-content: center;
+    width: 40vw;
+    margin: 5vh auto;
+    background: red;
+`;
+const FlexDiv = styled.div` flex: 1; `;
+const FlexLabel = styled.label` flex: 2; `;
+const FlexInput = styled.input` flex: 5; `;
 const Contact = () => {
     return (
         <div
@@ -11,7 +21,28 @@ const Contact = () => {
                 height: '100vh',
             }}
         >
-            <h1>Contact Section</h1>
+            <h1 style={{ paddingTop: '20vh' }}>Contact Section</h1>
+            {/* action="/pages/success" directs user to sucess page upon submission */}
+            <form name="contact-general" method="POST" data-netlify="true">
+                <FormRow>
+                    <FlexLabel>Your Name: </FlexLabel>
+                    <FlexDiv />
+                    <FlexInput type="text" name="name" id="name" />
+                </FormRow>
+                <FormRow>
+                    <FlexLabel>Your Email: </FlexLabel>
+                    <FlexDiv />
+                    <FlexInput type="text" name="name" id="email" />
+                </FormRow>
+                <FormRow>
+                    <FlexLabel>Message: </FlexLabel>
+                    <FlexDiv />
+                    <textarea style={{ flex: 5 }} name="message"></textarea>
+                </FormRow>
+                <FormRow>
+                    <button type="submit">Send</button>
+                </FormRow>
+            </form>
         </div>
     );
 };
