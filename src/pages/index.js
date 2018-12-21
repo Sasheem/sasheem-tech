1b09845870454873b4387254a4aae1996f65eeda
Sasheem <sts12@me.com>
2018-12-21 17:41:09 -0500

    comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Layout from '../components/layout'
 import Profile from '../components/Profile';
 // import About from '../components/About';
-import Projects from '../components/Projects';
+// import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
 const PageLessLayout = styled(Layout)`
