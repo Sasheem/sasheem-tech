8f6023276a90ddcc37c93549c76808263cfd1f12
Sasheem <sts12@me.com>
2018-12-29 16:56:44 -0500

    new card ui for projects section

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/code-icon.svg', // This path is relative to the root of the site.
+        icon: 'src/images/icon-code.svg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Card from './common/Card';
-import WebIcon from '../images/icon-web.svg';
-import MobileIcon from '../images/icon-mobile.svg';
+import WebIcon from '../images/icon_web_white_18dp.png';
+import MobileIcon from '../images/icon_iphone_white_18dp.png';
 
 // when screen width < 925px
 const ProjectContainer = styled.div`
@@ -21,12 +21,39 @@ const ProjectContainer = styled.div`
 const ProjectSection = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
+    grid-template-rows: 0.2fr 0.2fr;
     grid-column-gap: 2em;
     grid-row-gap: 2em;
-    padding: 3em;
+    padding: 5em;
     height: 80%;
     margin-bottom: 5em;
+`;
+
+const CardTop = styled.div`
+    display: flex;
+    padding: 1em;
+`;
+
+const CardBottom = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex: 4;
+    padding-left: 1em;
+    color: grey;
+`;
+
+const CardIcon = styled.img`
+    margin: 0 0 0 auto;
+    display: flex;
+    border: 1px solid black;
+    border-radius: 50%;
+    padding: 0.5em;
+    background: black;
+`;
+
+const CardTitle = styled.h4`
+    margin-top: 0.7em;
 `;
 
 const Projects = () => {
@@ -35,12 +62,33 @@ const Projects = () => {
             <h2>Previous Projects</h2>
             <ProjectSection>
                 <Card>
-                    <div style={{ display: 'flex' }}>
-                        <p>Project 1</p>
-                        <img src={WebIcon} alt="web icon" />
-                    </div>
-                    <div></div>
+                    <CardTop>
+                        <CardTitle>Information: Homelessness</CardTitle>
+                        <CardIcon src={WebIcon} alt="web icon" />
+                    </CardTop>
+                    <CardBottom>
+                        <p>HTML/CSS/Javascript - Flex</p>
+                    </CardBottom>
                 </Card>
+                <Card>
+                    <CardTop>
+                        <CardTitle>React App: Pictures</CardTitle>
+                        <CardIcon src={MobileIcon} alt="mobile icon" />
+                    </CardTop>
+                    <CardBottom>
+                        <p>React - Javascript - Unsplash API</p>
+                    </CardBottom>
+                </Card>
+                <Card>
+                    <CardTop>
+                        <CardTitle>Nonprofit: Unhoused Humanity</CardTitle>
+                        <CardIcon src={WebIcon} alt="web icon" /> 
+                    </CardTop>
+                    <CardBottom>
+                        <p>HTML/CSS/Javascript - SquareSpace</p>
+                    </CardBottom>
+                </Card>
+                
             </ProjectSection>
         </ProjectContainer>
     );
