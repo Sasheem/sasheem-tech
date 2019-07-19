3bec4cabe9fb86295dab5053f5ef2ff77690b60e
Sasheem <sts12@me.com>
2019-07-18 22:33:25 -0400

    altered content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import Image from './image';
-import ProjectLink from './ProjectLink';
+import Image from './image'
+import ProjectLink from './ProjectLink'
 
 // when screen < 925px
 const ProfileContainer = styled.div`
@@ -13,43 +13,47 @@ const ProfileContainer = styled.div`
   justify-content: center;
 
   @media only screen and (max-width: 925px) {
-      flex-direction: column-reverse;
-      justify-content: space-evenly;
-      height: 35em;
+    flex-direction: column-reverse;
+    justify-content: space-evenly;
+    height: 35em;
   }
-`;
+`
 // when screen < 925px
 const LeftContainer = styled.div`
   margin-right: 5em;
   @media only screen and (max-width: 925px) {
-      margin-right: 0;
-      text-align: center;
+    margin-right: 0;
+    text-align: center;
   }
-`;
+`
 // when screen < 925px
 const RightContainer = styled.div`
   margin-left: 5em;
   width: 300px;
   max-width: 300px;
   @media only screen and (max-width: 925px) {
-      margin-left: 0;
+    margin-left: 0;
   }
-`;
+`
 
 const Profile = () => {
-    return(
-        <ProfileContainer>
-            <LeftContainer>
-                <h1>Hi I'm Sasheem</h1>
-                <p>Mobile App and Web Developer</p>
-                <p style={{ marginBottom: '3em' }}>I build things with React and React Native</p>
-                <ProjectLink destination="https://www.unhousedhumanity.org/">Currently developing for Unhoused Humanity</ProjectLink>
-            </LeftContainer>
-            <RightContainer>
-                <Image />
-            </RightContainer>
-        </ProfileContainer>
-    );
-};
+  return (
+    <ProfileContainer>
+      <LeftContainer>
+        <h1>Hi I'm Sasheem</h1>
+        <p>Web Developer based out of Tallahssee, Florida</p>
+        <p style={{ marginBottom: '3em' }}>
+          I build websites with HTML5, CSS3, and Javascript(React)
+        </p>
+        <ProjectLink destination="https://www.unhousedhumanity.org/">
+          Currently developing for Unhoused Humanity
+        </ProjectLink>
+      </LeftContainer>
+      <RightContainer>
+        <Image />
+      </RightContainer>
+    </ProfileContainer>
+  )
+}
 
-export default Profile;
+export default Profile
